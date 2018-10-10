f7cf76765543e84441f1765677aff9388b66e76c
Craig Dunlop <craigdun9393@gmail.com>
2018-10-10 15:21:45 +0100

    updated revisions

--- models/cinema.js
+++ models/cinema.js
@@ -2,16 +2,20 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.filmTitles = function () {
+  return this.films.map(film => film.title);
+};
+
 Cinema.prototype.findByTitle = function (title) {
-return   this.films.filter(film => film.title === title)
+return   this.films.filter(film => film.title === title);
 };
 
 Cinema.prototype.findByGenre = function (genre) {
-return   this.films.filter(film => film.genre === genre)
+return   this.films.filter(film => film.genre === genre);
 };
 
 Cinema.prototype.findByYear = function (year) {
-return   this.films.filter(film => film.year === year)
+return   this.films.filter(film => film.year === year);
 };
 
 Cinema.prototype.checkByYear = function (year) {
--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -29,49 +29,49 @@ describe('Cinema', function () {
   });
 
   it('should be able to get a list of film titles',function(){
-    actual = cinema.films
+    const actual = cinema.films
     assert.deepStrictEqual(actual,[moonlight, bladeRunner, dunkirk, blackPanther, trainspotting])
   });
 
 
   it('should be able to find a film by title',function(){
-    actual = cinema.findByTitle("Blade Runner 2049");
+    const actual = cinema.findByTitle("Blade Runner 2049");
     assert.deepStrictEqual(actual, [bladeRunner])
   });
 
   it('should be able to filter films by genre',function(){
-    actual = cinema.filmsByProperty('genre','drama');
+    const actual = cinema.filmsByProperty('genre','drama');
     assert.deepStrictEqual(actual,[moonlight,trainspotting])
   });
 
   it('should be able to check whether there are some films from a particular year',function(){
-    actual = cinema.filmsByProperty('year',2016);
+    const actual = cinema.filmsByProperty('year',2016);
     assert.deepStrictEqual(actual,[moonlight])
   });
 
 
   it('should be able to check whether there are no films from a particular year',function(){
-    actual = cinema.checkByYear(1994);
+    const actual = cinema.checkByYear(1994);
     assert.strictEqual(actual,false)
   });
 
   it('should be able to check whether there are no films from a particular year--True',function(){
-    actual = cinema.checkByYear(2017);
+    const actual = cinema.checkByYear(2017);
     assert.strictEqual(actual,true)
   });
 
   it('should be able to check whether all films are over a particular length',function(){
-    actual = cinema.meetMinimumLength(90)
+    const actual = cinema.meetMinimumLength(90)
     assert.strictEqual(actual,true)
   });
 
   it('should be able to check whether all films are over a particular length--false',function(){
-    actual = cinema.meetMinimumLength(100)
+    const actual = cinema.meetMinimumLength(100)
     assert.strictEqual(actual,false)
   });
 
   it('should be able to calculate total running time of all films',function(){
-    actual= cinema.totalRunTime();
+    const actual= cinema.totalRunTime();
     assert.strictEqual(actual,622)
   });
 
